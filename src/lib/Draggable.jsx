6da5db65e1579260d1b5621f1f26d71118ef4bb8
maddrake220 <jwchoi4118@gmail.com>
2022-03-30 21:47:13 +0900

    feat: draggable test

---
+++ src/lib/Draggable.jsx
@@ -0,0 +1,64 @@
+import { useEffect, useRef, useState, useCallback, useMemo } from "react";
+import { debounce } from "underscore";
+
+const Draggable = ({ children, handleRef, onMove, x = 0, y = 0 }) => {
+  const dragRef = useRef(null);
+  const initialX = useRef(0);
+  const initialY = useRef(0);
+  const [position, setPosition] = useState({ x, y });
+
+  const Move = useMemo(() => debounce((x, y) => onMove(x, y), 500), [onMove]);
+
+  const onMouseMove = useCallback(
+    (event) => {
+      setPosition({
+        x: event.clientX - initialX.current,
+        y: event.clientY - initialY.current,
+      });
+      Move(event.clientX - initialX.current, event.clientY - initialY.current);
+    },
+    [Move]
+  );
+
+  const removeEvents = useCallback(() => {
+    document.removeEventListener("mousemove", onMouseMove);
+    document.removeEventListener("mouseup", removeEvents);
+    document.body.removeEventListener("mouseleave", removeEvents);
+  }, [onMouseMove]);
+
+  const onMouseDown = useCallback(
+    (event) => {
+      const { left, top } = dragRef.current.getBoundingClientRect();
+
+      // 도형내의  왼쪽, 위쪽 끝 점
+      initialX.current = event.clientX - left;
+      initialY.current = event.clientY - top;
+
+      document.addEventListener("mousemove", onMouseMove);
+      document.addEventListener("mouseup", removeEvents);
+      document.body.addEventListener("mouseleave", removeEvents);
+    },
+    [onMouseMove, removeEvents]
+  );
+
+  useEffect(() => {
+    const handle = handleRef.current;
+    handle.addEventListener("mousedown", onMouseDown);
+    return () => {
+      handle.removeEventListener("mousedown", onMouseDown);
+      Move.cancel();
+    };
+  }, [handleRef, onMouseDown, Move]);
+
+  return (
+    <div
+      ref={dragRef}
+      className="draggable"
+      style={{ transform: `translate(${position.x}px, ${position.y}px)` }}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Draggable;
